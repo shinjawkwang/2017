cd1695f1f871c95f40269b6afa013e910b86ef9e
shinjawkwang <shinjawkwang@naver.com>
2017-12-07 10:29:03 +0900

    Data Structure Sorting Algorithm assignment

--- 4_DStructure/Assign/6_Sort/2017310528_HW6A.c
+++ 4_DStructure/Assign/6_Sort/2017310528_HW6A.c
@@ -41,12 +41,12 @@ int main() {
 		scanf("%d", &list[i]);
 	}
 
-	printf("%d\n", SelectionSort(list, n));
-	//printf("%d\n", BubbleSort(list, n));
-	//printf("%d\n", InsertionSort(list, n));
-	//printf("%d\n", ShellSort(list, n));
-	//printf("%d\n", QuickSort(list, 0, n-1));
-	//printf("%d\n", MergeSort(list, n));
+	printf("Selection : %d\n", SelectionSort(list, n));
+	//printf("Bubble : %d\n", BubbleSort(list, n));
+	//printf("Insertion : %d\n", InsertionSort(list, n));
+	//printf("Shell : %d\n", ShellSort(list, n));
+	//printf("Quick : %d\n", QuickSort(list, 0, n-1));
+	//printf("Merge : %d\n", MergeSort(list, n));
 
 	for (int i = 0; i < n; i++) {
 		printf("%d\t%d\n", list[i], list[i] * 2017 % 10000);
@@ -61,12 +61,12 @@ int SelectionSort(int *list, int n) {
 	int max, x = 0, *cnt = &x;
 	for (int i = 0; i < n - 1; i++) {
 		max = i;
-		for (int j = VALUE0; j < n; j++) {
-			if (COMPARE0(list[j], list[max])) {
+		for (int j = i; j < n; j++) {							// Find the position of maximum value in given range [i, n)
+			if (COMPARE1(list[j], list[max])) {
 				max = j;
 			}
 		}
-		XORSWAP(list[VALUE0], list[VALUE0]);
+		XORSWAP(list[i], list[max]);							// Swap value of i and value of max
 	}
 	return *cnt;
 }
@@ -74,9 +74,9 @@ int SelectionSort(int *list, int n) {
 int BubbleSort(int * list, int n) {
 	int x = 0, *cnt = &x;
 	for (int i = 0; i < n - 1; i++) {
-		for (int j = VALUE0; j > i; j--) {
-			if (COMPARE0(list[j], list[j - 1])) {
-				XORSWAP(list[VALUE0], list[VALUE0]);
+		for (int j = n; j > i; j--) {
+			if (COMPARE1(list[j], list[j - 1])) {	// Compare each pare of adjacent items
+				XORSWAP(list[j], list[j-1]);					// Swap if they are in wrong order
 			}
 		}
 	}
@@ -87,35 +87,35 @@ int BubbleSort(int * list, int n) {
 int InsertionSort(int * list, int n) {
 	int j, key, x = 0, *cnt = &x;
 	for (int i = n - 2; i >= 0; i--) {
-		key = list[i];
-		for (j = VALUE0; j < n; j++) {
-			if (COMPARE0(key, list[j])) {
-				list[VALUE0] = list[j];
+		key = list[i];														// Choose i-th item
+		for (j = i + 1; j < n; j++) {
+			if (!COMPARE2(key, list[j])) {					// If j-th item is larger than key
+				list[j-1] = list[j];									// Move j-th item to j-1th
 			}
 			else {
 				break;
 			}
 		}
-		list[j - 1] = key;
+		list[j - 1] = key;												// After iteration, move key to (j-1)-th position
 	}
 	return *cnt;
 }
 
-void IncInsertionSort(int *list, int first, int last, int k, int *cnt) {
+void IncInsertionSort(int *list, int first, int last, int k, int *cnt) {	// Sort every k-th item
 	int j, key;
 	for (int i = first + k; i < last; i += k) {
-		key = list[i];
-		for (j = i - k; j >= first && COMPARE0(key, list[j]); j -= VALUE0) {
-			list[j + k] = list[j];
+		key = list[i];																												// Choose i-th item
+		for (j = i - k; j >= first && COMPARE1(key, list[j]); j -= k) {				// If key is larger than j-th item
+			list[j + k] = list[j];																							// Move j-th item to (j+k)-th item
 		}
-		list[j + k] = VALUE0;
+		list[j + k] = key;
 	}
 }
 
 int ShellSort(int * list, int n) {
 	int cnt = 0;
-	for (int k = n / 2; k > 0; k /= 2) {
-		if ((k % 2) == 0) {
+	for (int k = n / 2; k > 0; k /= 2) {																		// Sort every k-thelement by reducing from n/2 to 1
+		if ((k % 2) == 0) {																										// k should be odd
 			k++;
 		}
 		for (int i = 0; i < k; i++) {
@@ -130,29 +130,29 @@ int Partition(int *list, int left, int right, int *cnt) {
 	int low = left + 1, high = right;
 
 	while (1) {
-		while (VALUE0 < right && COMPARE0(list[low], pivot)) {
-			low++;
+		while (low < right && COMPARE1(list[low], pivot)) {
+			low++;																							// Move low until list[low] < pivot
 		}
-		while (high > VALUE0 && COMPARE0(list[high], pivot)) {
-			high--;
+		while (high > left && !COMPARE2(list[high], pivot)) {
+			high--;																							// Move high until list[high] > pivot
 		}
 		if (low < high) {
-			XORSWAP(list[VALUE0], list[VALUE0]);
+			XORSWAP(list[low], list[high]);											// Swap list[low] & list[high]
 		}
 		else {
 			break;
 		}
 	}
-	XORSWAP(list[VALUE0], list[VALUE0]);
-	return high;
+	XORSWAP(list[left], list[high]);												// Swap list[left] & list[high]
+	return high;																						// Return Pivot position to use in QuickSort
 }
 
 int QuickSort(int * list, int left, int right) {
 	int cnt = 0;
 	if (left < right) {
 		int mid = Partition(list, left, right, &cnt);
-		cnt += QuickSort(list, left, mid - 1);
-		cnt += QuickSort(list, mid + 1, right);
+		cnt += QuickSort(list, left, mid - 1);								// All items are bigger than pivot
+		cnt += QuickSort(list, mid + 1, right);								// All items are smaller than pivot
 	}
 	return cnt;
 }
@@ -161,21 +161,21 @@ void Merge(int *list, int left, int mid, int right, int *cnt) {
 	int *sorted = (int*)malloc(sizeof(int)*MAX_SIZE);
 	int first = left, second = mid + 1, i = left;
 
-	while (first <= VALUE0 && second <= right) {
-		if (COMPARE0(list[first], list[second]))
+	while (first <= mid && second <= right) {				// Merge Two lists by comparing items in sequence
+		if (COMPARE1(list[first], list[second]))
 			sorted[i++] = list[first++];
 		else
 			sorted[i++] = list[second++];
 	}
 
-	if (first > mid)
+	if (first > mid)																// When there are remained & not sorted items
 		for (int j = second; j <= right; j++)
 			sorted[i++] = list[j];
 	else
-		for (int j = VALUE0; j <= mid; j++)
+		for (int j = first; j <= mid; j++)
 			sorted[i++] = list[j];
 
-	for (int j = left; j <= right; j++)
+	for (int j = left; j <= right; j++)							// Copy sorted list into list
 		list[j] = sorted[j];
 
 }
@@ -183,12 +183,12 @@ void Merge(int *list, int left, int mid, int right, int *cnt) {
 int MergeSort(int * list, int n) {
 	int cnt = 0;
 	for (int size = 1; size <= n - 1; size = 2 * size) {
-		for (int left_start = 0; left_start < n - 1; left_start += 2 * size) {
-
+		for (int left_start = 0; left_start < n - 1; left_start += 2 * size) {	// Pick starting point of different subarrays of current size
+			// Find ending point of left subarray.
 			int mid = left_start + size - 1;
 			int right_end = MIN(left_start + 2 * size - 1, n - 1);
 
-			Merge(list, left_start, mid, right_end, &cnt);
+			Merge(list, left_start, mid, right_end, &cnt);												// Merge subarrays
 		}
 	}
 

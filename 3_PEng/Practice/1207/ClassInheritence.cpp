11e087861973279bd8d46da7fb1c4de7763e0f74
shinjawkwang <shinjawkwang@naver.com>
2017-12-08 17:38:44 +0900

    CPE Assignment1 - inheritent clas clear

--- 3_PEng/Practice/1207/ClassInheritence.cpp
+++ 3_PEng/Practice/1207/ClassInheritence.cpp
@@ -9,7 +9,7 @@ public:
   Person(string str);
   Person(const Person& person);
   ~Person();
-  string getName(){
+  string getName() const{
     return name;
   }
 private:
@@ -22,24 +22,23 @@ Person::Person(string str){
   name = str;
   cout << "Person(string) called\n";
 }
-Person::Person(const Person& person){
-  name = person.name;
+Person::Person(const Person& person):name(person.name){
   cout << "Person(const Person&) called\n";
 }
 Person::~Person(){
-  free(this);
+  //free(this);
   cout << "~Person() called\n";
 }
 /* Person - Student Class */
 class Student : public Person{
 public:
   Student();
-  Student(Person person);
+  Student(const Person& person);
 };
 Student::Student():Person(){
   cout << "Student() called\n";
 }
-Student::Student(Person person):Person(person.getName()){
+Student::Student(const Person& person):Person(person.getName()){
   cout << "Student(const Person&) called\n";
 }
 /* Person - Worker Class */
@@ -47,32 +46,61 @@ class Worker : public Person{
 public:
   Worker();
   Worker(string str);
-  Worker(Person person);
-  ~Worker();
+  Worker(const Person& person);
+  virtual ~Worker();
 };
 Worker::Worker():Person(){
   cout << "Worker() called\n";
 }
-Worker::Worker(string str){
+Worker::Worker(string str):Person(str){
   cout << "Worker(string) called\n";
 }
-
+Worker::Worker(const Person& person):Person(person.getName()){
+  cout << "Worker(const Person&) called\n";
+}
+Worker::~Worker(){
+  cout << "~Worker() called\n";
+}
+/* Person - Worker - Doctor Class */
 class Doctor : public Worker{
 public:
   Doctor();
   Doctor(string str);
   Doctor(const Person& person);
 };
+Doctor::Doctor():Worker(){
+  cout << "Doctor() called\n";
+}
+Doctor::Doctor(string str):Worker(str){
+  cout << "Doctor(string) called\n";
+}
+Doctor::Doctor(const Person& person):Worker(person.getName()){
+  cout << "Doctor(const Person&) called\n";
+}
+/* Person - Worker - Doctor - Manager Class */
 class Manager : public Doctor{
 public:
   Manager(const Person& person);
+  ~Manager();
 };
+Manager::Manager(const Person& person):Doctor(person.getName()){
+  cout << "Manager(const Person&) called\n";
+}
+Manager::~Manager(){
+  cout << "~Manager() called\n";
+}
+/* Person - Patient Class */
 class Patient : public Person{
 public:
   Patient();
   Patient(string str);
 };
-
+Patient::Patient():Person("Default String"){
+  cout << "Patient() called\n";
+}
+Patient::Patient(string str):Person(str){
+  cout << "Patient(string) called\n";
+}
 
 int main()
 {
@@ -88,68 +116,22 @@ int main()
     cout << "--" << endl;
 
 		//----------- Insert your code after here ---//
+    Doctor* doctor3 = new Doctor();
+    delete doctor3;
+    cout << "--\n";
+    Patient patient2;
+    cout << "--\n";
+    Person person1;
+    cout << "--\n";
+    Worker* worker1 = new Worker(person1);
+    delete worker1;
+    cout << "--\n";
+    Person person2(patient2);
+    cout << "--\n";
+    cout << "--\n";
+    Student student2(person1);
+    cout << "--\n";
+    Worker worker2;
 
     return 0;
 }
-
-/*  Expected output
-Person(string) called << 1
-Patient(string) called
---
-Person() called << 2
-Worker() called << 1
-Doctor() called
---
-Person(string) called << 3
-Worker(string) called << 2
-Doctor(const Person&) called
---
-Person(string) called << 4
-Worker(string) called << 3
-Doctor(string) called
-Manager(const Person&) called << 1
---
-Person() called << 5
-Student() called
---
-Person() called << 6
-Worker() called << 4
-Doctor() called
-~Worker() called << 3
-~Person() called << 5
---
-Person(string) called << 6
-Patient() called
---
-Person() called << 7
---
-Person(string) called << 8
-Worker(const Person&) called << 4
-~Worker() called << 3
-~Person() called << 7
---
-Person(const Person&) called << 8
---
---
-Person(string) called << 9
-Student(const Person&) called
---
-Person() called << 10
-Worker() called << 4
-~Worker() called << 3
-~Person() called << 9
-<<
-~Person() called << 8
-~Person() called << 7
-~Person() called << 6
-~Person() called << 5
-~Person() called << 4
-~Manager() called << 0
-~Worker() called << 2
-~Person() called << 3
-~Worker() called << 1
-~Person() called << 2
-~Worker() called << 0
-~Person() called << 1
-~Person() called << 0
-*/
